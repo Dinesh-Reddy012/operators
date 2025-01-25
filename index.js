7f092e2ce05edb0680d230846a40fac3684ec580
Dinesh Reddy <dineshreddy8844@gmail.com>
2025-01-25 12:05:35 +0530

    op

---
+++ index.js
@@ -0,0 +1,33 @@
+console.log(10+'5');
+let x = 10;
+console.log(x+=5);
+console.log(x-null);
+console.log(x*=2);
+console.log(x/'2');
+console.log(x/3);
+console.log(x**2);
+console.log(10>5 && 5<10);
+console.log(x==='x');
+console.log(null??'default');
+console.log(x||'default');
+console.log(x & 5);
+console.log(x | 5);
+console.log(x ^ 5);
+let y = ++x;
+console.log(x,y);
+console.log(++x);
+console.log(x++);
+y = --x;
+console.log(x,y);
+(y=x>5)?console.log("yes"):console.log("no");
+(y=x>5)?console.log("yes"):x>3?console.log("maybe"):console.log("no")
+
+
+
+
+
+
+
+
+
+
